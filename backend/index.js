5593892b985d3e02d6b952c8e518d65dbb2ccf82
Javier Triana <jatriacos@gmail.com>
2021-11-16 18:00:16 -0500

    CRUD
    Buscar total, parcial, actualizar total, parcial, eliminar

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,24 @@
+/*forma para importar varialbes o funciones únicas 
 const traerSemov = require("./src/db/crud.js");
+leerSemovientes(); */
+const {leerSemovientes} = require("./src/db/crud.js");
 
-traerSemov();
-/* escribir(); */
+const express = require('express');
+const app = express(); // se crea una aplicación de express utilizando la librería
+const port = 3000;  //definición del puerto
+
+app.get('/get-semovientes',function(req, res){
+    leerSemovientes(function(arraySemovientes){
+        res.send(arraySemovientes);
+    })
+})
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log("Corriendo en el puerto " + port)
+})
--- backend/src/db/crud.js
+++ backend/src/db/crud.js
@@ -1,33 +1,111 @@
 const db = require('./firebase.js');
 
- //traer todos los semovientes
- function traerSemov(){
-    db.collection("semovientes").get().then((docs) => {
+ //leer todos los semovientes
+ function leerSemovientes(callback){
+    return db.collection("semovientes").get().then((docs) => {
+        var arraySemovientes = [];
         docs.forEach((user) => {
-            console.log(user.data());
+            //console.log(user.data()); las líneas 6-9-12 no existian
+            arraySemovientes.push(user.data());
         })
+        //Aquí se envía la respuesta al GET REQUEST
+        callback(arraySemovientes);
+    }).catch(() => {
+        callback(`Error para obtener semovientes ${error}`);
     })
     }
+
+ //leer un semoviente en específico
+ function leerSemoviente(uid, callback){
+     return db.collection("semovientes").doc(uid).get()
+     .then ((refDoc) => {
+         callback(refDoc.data())
+     })
+ }   
 
 //Ingresa los semovientes
-/* function escribir(){
-    db.collection("semovientes").add({
-        id: "are-01",
-        lote: "antiguo",
-        fecha_compra: 02/02/2021,
-        compra: 'true',
-        edad: 23,
-        castrado: false,
-        sexo:"hembra"
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
+function ingresarSemoviente(semoviente, callback){
+    return db.collection("semovientes").add(semoviente)
+    .then(() => {
+        callback("HECHO!!!");
     })
     .catch((error) => {
-        console.error("Error adding document: ", error);
+        callback(`Error adicionando información ${error}`);
     });
     }
- */
 
-module.exports = traerSemov;
-/* module.exports = escribir; */
+function actualizarSemovienteTotalmente(uid, semoviente, callback){
+    return db.collection("semovientes").doc(uid).set(semoviente)
+        .then(() =>{
+            callback("Hecho");
+        })
+        .catch((error) => {
+            callback(`Error adicionando información ${error}`);
+        });   
+}
+
+function actualizarSemovienteParcial(uid, semoviente, callback){
+    return db.collection("semovientes").doc(uid).update(semoviente)
+    .then(() =>{
+        callback("Hecho");
+    })
+    .catch((error) => {
+        callback(`Error adicionando información ${error}`);
+    });   
+}
+
+function borrarSemoviente(uid, callback){
+    return db.collection("semovientes").doc(uid).delete()
+    .then(() => {
+        callback("Eliminado el muchacho");
+    })
+    .catch((error) => {
+        callback(`Error eliminando información ${error}`);
+    });   
+}
+//PRUEBAS
+//prueba para ver si se está leyendo el crud PASANDO UN id
+/* leerSemoviente("rGCo8yEttRcNUfaBD3gJ", (vaquitas) => {
+    console.log(vaquitas)
+}) */
+///////
+// prueba para inggresar una vaquita NUEVA
+/* const semoviente = {
+    id: "are-04",
+    lote: "nuevo",
+    fecha_compra: 02/07/2021,
+    compra: 'true',
+    edad: 18,
+    castrado: false,
+    sexo:"macho"
+}
+ingresarSemoviente(semoviente, (status)=>{
+    console.log(status);  
+})
+ */
+//Para actualizar un Semoviente Totalmente
+/* const semoviente = {
+    "id": "Vlyva-01",
+    "lote": "viejito",
+    "edad": 35
+    }
+actualizarSemovienteTotalmente("DlShLjhTbyKD0I1eGgCu", semoviente, (status) =>{
+    console.log(status)
+})
+ */
+// Para eliminar un semovientes
+borrarSemoviente("DlShLjhTbyKD0I1eGgCu", (status) =>{
+    console.log(status)
+})
+
+
+
+module.exports = {
+    leerSemovientes,
+    leerSemoviente, 
+    ingresarSemoviente, 
+    actualizarSemovienteTotalmente,
+    actualizarSemovienteParcial,
+    borrarSemoviente
+
+}
